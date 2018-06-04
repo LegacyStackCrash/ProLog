164801db1217cf46deb888d4de2dea80f2ba4108
= <chase.terry87@gmail.com>
2018-06-04 09:14:25 -0500

    Adjusted customer and departments factories to give them unique names.

--- database/factories/CustomersFactory.php
+++ database/factories/CustomersFactory.php
@@ -4,13 +4,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Customers::class, function (Faker $faker) {
 
-    $city = $faker->city();
+    $city = $faker->unique()->city();
     $agency = ['PD', 'SO', 'FD'];
 
     return [
         'customer_name' => $city.' '.$agency[rand(0,2)],
         'customer_city' => $city,
         'customer_state' => $faker->stateAbbr(),
-        'customer_phone' => substr($faker->phoneNumber(),0, 15),
+        'customer_phone' => substr($faker->tollFreePhoneNumber(),0, 15),
     ];
 });
--- database/factories/DepartmentsFactory.php
+++ database/factories/DepartmentsFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Departments::class, function (Faker $faker) {
     return [
-        'department_name' => ucfirst($faker->word())
+        'department_name' => ucfirst($faker->unique()->word())
     ];
 });
