7c612a462f8dfe286b16b59cb6d9b00dd008f130
= <chase.terry87@gmail.com>
2018-06-07 15:02:42 -0500

    Better formatting for mobile navigation.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -23,11 +23,14 @@
 
                 <ul id="nav-mobile" class="sidenav">
                     <li class="{{ Request::segment(1) == '' ? 'active' : '' }}"><a href="/home">Home</a></li>
+                    <li class="divider"></li>
                     <li class="{{ Request::segment(1) == 'users' ? 'active' : '' }}"><a href="/users">Users</a></li>
                     <li class="{{ Request::segment(1) == 'departments' ? 'active' : '' }}"><a href="/departments">Departments</a></li>
                     <li class="{{ Request::segment(1) == 'customers' ? 'active' : '' }}"><a href="/customers">Customers</a></li>
                     <li class="{{ Request::segment(1) == 'projects' ? 'active' : '' }}"><a href="/projects">Projects</a></li>
                     <li class="{{ Request::segment(1) == 'issues' ? 'active' : '' }}"><a href="/issues">Issues</a></li>
+                    <li class="divider"></li>
+                    <li><a href="/user/settings">Settings</a></li>
                     <li><a href="/logout">Log Out</a></li>
                 </ul>
                 <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
